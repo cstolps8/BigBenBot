765e93c55896fa9be7243abe2e65a35713d7d8c0
cstolps8 <collin.stolpa@gmail.com>
2021-06-28 22:09:15 -0500

    get voicechanel data

--- bot.js
+++ bot.js
@@ -4,15 +4,18 @@ const cron = require('node-cron');
 
 const { TOKEN, VOICE_CHANNEL_ID, GUILD_ID, TEXT_CHANNEL_ID } = process.env;
 
+
 const Client = new Discord.Client();
 
-let guild, voiceChannel, textChannel ;
+let guild, voiceChannel, textChannel;
+let vcArr = []
 
 // When bot comes online check the guild and voice channel are valid
 // if they are not found the program will exit
 Client.on('ready', async () => {
 	try {
-		guild = await Client.guilds.fetch(GUILD_ID);
+		guild = await Client.guilds.fetch(GUILD_ID)
+		getFullestVoiceChannel()
 		voiceChannel = guild.channels.cache.get(VOICE_CHANNEL_ID);
 	} catch (error) {
 		console.log(error);
@@ -23,15 +26,18 @@ Client.on('ready', async () => {
 });
 
 // use node-cron to create a job to run every hour
-const task = cron.schedule('0 0 */1 * * *', async () => {
+//const task = cron.schedule('0 0 */1 * * *', async () => {
+	//debugging uncomment next line
+const task = cron.schedule('5 * * * * *', async () => {
+
 	let { hour, amPm, timezoneOffsetString } = getTimeInfo();
 
 	// if text channel was defined send message in chat
 	if (textChannel) {
 		const messageEmbed = new Discord.MessageEmbed()
-		.setColor('#FFD700')
-		.setTitle(`The time is now ${hour}:00 ${amPm} GMT${timezoneOffsetString}`)
-		
+			.setColor('#FFD700')
+			.setTitle(`The time is now ${hour}:00 ${amPm} GMT${timezoneOffsetString}`)
+
 		textChannel.send(messageEmbed);
 	}
 
@@ -42,37 +48,54 @@ const task = cron.schedule('0 0 */1 * * *', async () => {
 			const connection = await voiceChannel.join();
 			// counter for looping
 			let count = 1;
-		
+
 			// immediately invoked function that loops to play the bell sound 
 			(function play() {
 				connection.play('bigben.mp3')
-				.on('finish', () => {
-					count += 1;
-					if (count <= hour) {
-						play();
-					} else {
-						connection.disconnect();
-					}
-				})
+					.on('finish', () => {
+						count += 1;
+						if (count <= hour) {
+							play();
+						} else {
+							connection.disconnect();
+						}
+					})
 			})();
 
-		} catch(error) {
+		} catch (error) {
 			console.log(error);
 		}
 	}
 });
 
+//function to find the voice channel with the most users in it 
+const  getFullestVoiceChannel = () => {
+
+	//get all voice channel ids
+	// find voice channel with most users in it 
+	// if voice channel has same number of users
+	// if voice channel has no users in it dont play music
+	guild.channels.cache.each(voiceChannel => {
+
+		if (voiceChannel.type == "voice" && voiceChannel.members.array().length >= 1) {
+			console.log(voiceChannel.name + " has " + voiceChannel.members.array().length + " users in it ")
+			vcArr.push(voiceChannel.id)
+
+		}
+	});
+}
+
 // function to get current time and return object containing
 // hour and if it is am or pm
 const getTimeInfo = () => {
-		let time = new Date();
-		let hour = time.getHours() >= 12 ? time.getHours() - 12 : time.getHours();
-		hour = hour === 0 ? 12 : hour;
-		let amPm = time.getHours() >= 12 ? 'PM' : 'AM';
-		// get gmt offset in minutes and convert to hours
-		let gmtOffset = time.getTimezoneOffset() / 60
-		// turn gmt offset into a string representing the timezone in its + or - gmt offset
-		let timezoneOffsetString = `${gmtOffset > 0 ? '-':'+'} ${Math.abs(gmtOffset)}`;
+	let time = new Date();
+	let hour = time.getHours() >= 12 ? time.getHours() - 12 : time.getHours();
+	hour = hour === 0 ? 12 : hour;
+	let amPm = time.getHours() >= 12 ? 'PM' : 'AM';
+	// get gmt offset in minutes and convert to hours
+	let gmtOffset = time.getTimezoneOffset() / 60
+	// turn gmt offset into a string representing the timezone in its + or - gmt offset
+	let timezoneOffsetString = `${gmtOffset > 0 ? '-' : '+'} ${Math.abs(gmtOffset)}`;
 
 	return {
 		hour,
