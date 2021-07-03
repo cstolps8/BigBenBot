e5c99abf2225a507ecec4cf6b54f0f95372d06a5
cstolps8 <collin.stolpa@gmail.com>
2021-07-03 13:42:38 -0500

    adding bot stuff

--- bot.js
+++ bot.js
@@ -8,16 +8,22 @@ const { TOKEN, VOICE_CHANNEL_ID, GUILD_ID, TEXT_CHANNEL_ID } = process.env;
 const Client = new Discord.Client();
 
 let guild, voiceChannel, textChannel;
-let vcArr = []
+let vcArr
 
 // When bot comes online check the guild and voice channel are valid
 // if they are not found the program will exit
 Client.on('ready', async () => {
 	try {
 		guild = await Client.guilds.fetch(GUILD_ID)
-		let VOICE_CHANNEL_ID = await getFullestVoiceChannel()
-		console.log("This channel has the most users in it " + VOICE_CHANNEL_ID)
-		voiceChannel = guild.channels.cache.get(VOICE_CHANNEL_ID);
+
+		guild.channels.cache.each((values) => {
+
+			if (values.type == "voice" && values.members.array().length >= 1) {
+				console.log(values.name + " has " + values.members.array().length + " users in it ")
+				vcArr = values.id
+			}
+		});
+		voiceChannel = guild.channels.cache.get(vcArr);
 	} catch (error) {
 		console.log(error);
 		process.exit(1);
@@ -29,17 +35,28 @@ Client.on('ready', async () => {
 // use node-cron to create a job to run every hour
 // const task = cron.schedule('0 0 */1 * * *', async () => {
 //debugging uncomment next line
-//const task = cron.schedule('5 * * * * *', async () => {
-Client.on('message', message => {
-	let connect = Client.channels.cache.get('793901584060514354')
-	if (message.content.startsWith("join")) {
-		const connection = message.member.voice.channel.join();
-	}
-	if (message.content.startsWith("leave")) {
-		console.log("leaving")
-		const connection = message.member.voice.channel.leave();
-	}
+const task = cron.schedule('5 * * * * *', async () => {
+	console.log("----RUNNING BOT----")
+// Client.on('message', message => {
+// 	let connect = Client.channels.cache.get('793901584060514354')
+// 	if (message.content.startsWith("join")) {
+// 		const connection = message.member.voice.channel.join();
+// 	}
+// 	if (message.content.startsWith("leave")) {
+// 		console.log("leaving")
+// 		const connection = message.member.voice.channel.leave();
+// 	}
 
+	guild = await Client.guilds.fetch(GUILD_ID)
+	guild.channels.cache.each((values) => {
+
+		if (values.type == "voice" && values.members.array().length >= 1) {
+			console.log(values.name + " has " + values.members.array().length + " users in it ")
+			vcArr = values.id
+		}
+	});
+
+	voiceChannel =  guild.channels.cache.get(vcArr);
 
 
 
@@ -55,10 +72,11 @@ Client.on('message', message => {
 	}
 
 	// check if VC defined in config is empty
+	console.log("Voice Channel = " + voiceChannel)
 	if (voiceChannel.members.size >= 1) {
 		try {
 			// connect to voice channel
-			const connection =  voiceChannel.join();
+			const connection = await voiceChannel.join();
 			// counter for looping
 			let count = 1;
 
@@ -70,7 +88,8 @@ Client.on('message', message => {
 						if (count <= hour) {
 							play();
 						} else {
-							connection.disconnect();
+							console.log("--LEAVING--")
+							 connection.disconnect();
 						}
 					})
 			})();
@@ -81,23 +100,6 @@ Client.on('message', message => {
 	}
 });
 
-//function to find the voice channel with the most users in it 
-const getFullestVoiceChannel = () => {
-
-	//get all voice channel ids
-	// find voice channel with most users in it 
-	// if voice channel has same number of users
-	// if voice channel has no users in it dont play music
-	guild.channels.cache.each(voiceChannel => {
-
-		if (voiceChannel.type == "voice" && voiceChannel.members.array().length >= 1) {
-			console.log(voiceChannel.name + " has " + voiceChannel.members.array().length + " users in it ")
-			//vcArr.push(voiceChannel.id)
-			return voiceChannel.id
-
-		}
-	});
-}
 
 // function to get current time and return object containing
 // hour and if it is am or pm
@@ -119,6 +121,6 @@ const getTimeInfo = () => {
 }
 
 // start the cron job
-//task.start();
+task.start();
 
 Client.login(TOKEN);
